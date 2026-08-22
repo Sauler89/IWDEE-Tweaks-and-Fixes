caaed0221f5a2311bf429e3c3975ba0edc8d4aa8
Sauler <138236347+Sauler89@users.noreply.github.com>
2026-08-22 10:18:21 +0200

    Clarify Chromatic Dye Kit color family labels

---
+++ IWDEE-Tweaks-and-Fixes/dialogs/iwtfdye_family_labels.d
@@ -0,0 +1,79 @@
+// Clarify that these menu entries are palette families, not body-part limits.
+// State 1 is the Categories state in IWTFDYE.DLG.
+
+ALTER_TRANS IWTFDYE
+BEGIN 1 END
+BEGIN 0 END
+BEGIN
+  "REPLY" ~Hair color family~
+END
+
+ALTER_TRANS IWTFDYE
+BEGIN 1 END
+BEGIN 1 END
+BEGIN
+  "REPLY" ~Skin color family I~
+END
+
+ALTER_TRANS IWTFDYE
+BEGIN 1 END
+BEGIN 2 END
+BEGIN
+  "REPLY" ~Skin color family II~
+END
+
+ALTER_TRANS IWTFDYE
+BEGIN 1 END
+BEGIN 3 END
+BEGIN
+  "REPLY" ~Leather color family~
+END
+
+ALTER_TRANS IWTFDYE
+BEGIN 1 END
+BEGIN 4 END
+BEGIN
+  "REPLY" ~Metal color family~
+END
+
+ALTER_TRANS IWTFDYE
+BEGIN 1 END
+BEGIN 5 END
+BEGIN
+  "REPLY" ~Cloth color family - earth tones~
+END
+
+ALTER_TRANS IWTFDYE
+BEGIN 1 END
+BEGIN 6 END
+BEGIN
+  "REPLY" ~Cloth color family - warm tones~
+END
+
+ALTER_TRANS IWTFDYE
+BEGIN 1 END
+BEGIN 7 END
+BEGIN
+  "REPLY" ~Cloth color family - green and aqua~
+END
+
+ALTER_TRANS IWTFDYE
+BEGIN 1 END
+BEGIN 8 END
+BEGIN
+  "REPLY" ~Cloth color family - blue and violet~
+END
+
+ALTER_TRANS IWTFDYE
+BEGIN 1 END
+BEGIN 9 END
+BEGIN
+  "REPLY" ~Cloth color family - neutrals~
+END
+
+ALTER_TRANS IWTFDYE
+BEGIN 1 END
+BEGIN 10 END
+BEGIN
+  "REPLY" ~Special / elemental color family~
+END
